1b91de9fd2b742e005d1a298a4e765835f15eb12
DBVeit <43188547+DBVeit@users.noreply.github.com>
2020-04-17 16:49:47 -0300

    Função para autenticação do usuário
    **Atualizado 17/04/2020 - Adicionada a função md5 na query select; Adicionada a verificação da confirmação do usuário: se o mesmo tentar logar sem realizar o procedimento de confirmação da conta por email, o acesso não será liberado.

--- php/functions/autentica-usuario.php
+++ php/functions/autentica-usuario.php
@@ -1,25 +1,28 @@
 <?php
-
+session_start();
 include_once("../conexao.php");
 
 $password = $_POST["password"];
 $email = $_POST["email"];
 
-$result = mysqli_query($mysqli, "SELECT * FROM pessoa WHERE email='$email' AND password='$password'");
+$result = mysqli_query($mysqli, "SELECT * FROM pessoa WHERE email='$email' AND password=md5('$password')");
 $user_data = $result->fetch_assoc();
 
 $row = mysqli_num_rows($result);
 
 if($row == 1){
-
-    $_SESSION['email'] = $email;
-    $_SESSION['name'] = $user_data['name'];
-
-    header('Location: ../../sucesso.php');
+	if($user_data['confirmacao'] != 0){
+		$_SESSION['email'] = $email;
+    	$_SESSION['name'] = $user_data['name'];
+    	header('Location: sucesso.php');
+	}else{
+		$_SESSION['nao_confirmado'] = true;
+		header('Location: ../../index.php');
+		exit();
+	}
 }else{
+	$_SESSION['nao_autenticado'] = true;
     header('Location: ../../index.php');
+    exit();
 }
-
-
-
 ?>
