06293757bdf12d639c576377afde41b82b9ef3d8
DBVeit <43188547+DBVeit@users.noreply.github.com>
2020-05-13 17:32:24 -0300

    Sidenav c/ nome de usuário

--- pages/main.php
+++ pages/main.php
@@ -27,6 +27,23 @@ $row = $result->fetch_assoc();
 
 </head>
 <body class="displayMovie">
+  <ul id="slide-out" class="sidenav">
+    <li><div class="user-view">
+      <div class="background">
+        <img src="images/office.jpg">
+      </div>
+      <a href="#user"><img class="circle" src="images/yuna.jpg"></a>
+      <a href="#name"><span class="white-text name"><?php echo $row['name']; ?></span></a>
+      <a href="#email"><span class="white-text email"><?php echo $_SESSION['name']; ?></span></a>
+    </div></li>
+    <li><a href="#!"><i class="material-icons">cloud</i>First Link With Icon</a></li>
+    <li><a href="#!">Second Link</a></li>
+    <li><div class="divider"></div></li>
+    <li><a class="subheader">Subheader</a></li>
+    <li><a class="waves-effect" href="#!">Third Link With Waves</a></li>
+  </ul>
+  <a href="#" data-target="slide-out" class="sidenav-trigger"><i class="material-icons">menu</i></a>
+
   <nav class="grey darken-3 lighten-1 mat-header"  role="navigation" >
     <div class="nav-wrapper container"><a id="logo-container" href="../index.php" class="brand-logo" ><i class="material-icons" style="font-size: 39px;">play_circle_filled</i>WatchIt!</a>
       <ul class="right hide-on-med-and-down">
